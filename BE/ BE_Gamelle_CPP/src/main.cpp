621ab291e6eb4d6394e8492959d747f00736bde0
Cedric <cedric.chanfreau@gmail.com>
2023-12-16 00:48:34 +0100

    Gamelle_V1

--- BE/ BE_Gamelle_CPP/src/main.cpp
+++ BE/ BE_Gamelle_CPP/src/main.cpp
@@ -2,6 +2,7 @@
 #include "Servomotor.h"
 #include "Alerte.h"
 #include <iostream>
+#include <vector>
 
 // WiFiLEDServer server("Redmi Note 10 Pro", "totototo", 15);
 // WiFiLEDServer server("Les Restos De La Coo", "KB6zDjij");
@@ -9,11 +10,13 @@
 // WiFiLEDServer server("Zyxel_BA41", "Lord&Lady");
 WiFiLEDServer server("Livebox-8C50", "Chanfreaulafamille5");
 // Pin pour le servo
+
 const int brocheServoG = 12; // D6
 const int brocheServoD = 5;  // D1
 const int brocheCapteur = 13; // D7
 Buzzer monBuzzer(D5); // D5
-LED maLED(D8); //D8
+//LED maLED(D8); //D8
+std::vector<LED> leds;
 int previouschoix = 0;
 
 ServoMoteur monServoMoteurG(brocheServoG);
@@ -27,6 +30,15 @@ void setup()
 
   monServoMoteurG.AllumerActionneur(true);
   monServoMoteurD.AllumerActionneur(true);
+
+  leds.push_back(LED(D8)); // LED rouge
+  leds.push_back(LED(D3)); // LED jaune
+  leds.push_back(LED(D4)); // LED verte
+  for (LED& led : leds) {
+    led.activer();
+    delay(1000);
+    led.desactiver();
+  }
 
   std::list<Croquette> maListeDecroquette;
   Croquette croquetteG;
@@ -49,7 +61,10 @@ void loop()
   if (monCapteurDepression.estEnfonce())
   {
     monBuzzer.desactiver();
-    maLED.desactiver();
+    for (LED& led : leds) {
+    led.desactiver();
+  }
+
     Serial.println("Capteur appuye");
 
     if (server.choix == 0)
@@ -61,6 +76,7 @@ void loop()
     else if (server.choix == 1)
     {
       Serial.println("choix 1");
+      leds[1].activer();
       monServoMoteurD.tourner(180);
       delay(1000);
       server.choix = 0;
@@ -68,6 +84,7 @@ void loop()
     else if (server.choix == 2)
     {
       Serial.println("choix 2");
+      leds[2].activer();
       monServoMoteurG.tourner(180);
       delay(1000);
       server.choix = 0;
@@ -75,7 +92,7 @@ void loop()
   } else if((!monCapteurDepression.estEnfonce() && server.choix == 1) || (!monCapteurDepression.estEnfonce() && server.choix == 2)){
     server.choix = 0;
     monBuzzer.activer();
-    maLED.activer();
+    leds[0].activer();
   }
   else
   {
